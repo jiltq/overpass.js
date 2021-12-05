2ba467c04cee1df5d3950e28c258224a8a321810
Ethan <ejpinson1@gmail.com>
2021-12-05 17:55:02 -0600

    insert legitimate commit name here

--- src/OverpassQuery.js
+++ src/OverpassQuery.js
@@ -10,22 +10,54 @@ class OverpassQuery {
 		this.format = 'json';
 		this.timeout = 180;
 	}
+	/**
+   	 * Options for utilizing an element
+     * @typedef {Object} ElementOptions
+     * @property {string} [type] Type of element
+     * @property {Object[]} [tags] Tags associated with the element
+     * @property {number[]} [bbox] Bounding box for the element
+     */
+
+	/**
+	 * Add an element to the query
+	 * @param {ElementOptions} options Options for the element
+	 * @returns {OverpassQuery}
+	 */
 	addElement({ type, tags, bbox }) {
 		const elementString = `${type}${tags.map(tag => `[${Object.keys(tag)}=${Object.values(tag)}](${bbox.join(',')})`)}`;
 		this.elements.push(elementString);
 		return this;
 	}
+	/**
+	 * Sets the format of the query
+	 * @param {string} format Desired format
+	 * @returns {OverpassQuery}
+	 */
 	setFormat(format) {
 		this.format = format;
 		return this;
 	}
+	/**
+	 * Sets the timeout of the query
+	 * @param {number} format Desired timeout
+	 * @returns {OverpassQuery}
+	 */
 	setTimeout(timeout) {
 		this.timeout = timeout;
 		return this;
 	}
+	/**
+	 * The URL of the query
+	 * @returns {string}
+	 * @readonly
+	 */
 	get query() {
 		return `${mainURL}[out:${this.format}][timeout:${this.timeout}];(${this.elements.join(';')};);out body;>;out skel qt;`;
 	}
+	/**
+	 * Fetches the query
+	 * @returns {any}
+	 */
 	async fetch() {
 		const response = await nodefetch(this.query);
 		return JSON.parse(JSON.stringify(await (response).json()));
